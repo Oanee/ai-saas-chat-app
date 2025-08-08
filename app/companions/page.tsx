1cce1f0e2f8360d34abc8b8a4748829949f1b310
Alin Oancea <alin.oancea@keepcalling.com>
2025-08-08 17:05:42 +0300

    implement the vapi ai assistant

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,35 @@
-const CompanionsLibrary = async () => {
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import CompanionCard from "@/components/CompanionCard";
+import { getSubjectColor } from "@/lib/utils";
+import SubjectFilter from "@/components/SubjectFilter";
+import SearchInput from "@/components/SearchInput";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+
   return (
-    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <h1>Companions</h1>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
     </main>
   );
 };
